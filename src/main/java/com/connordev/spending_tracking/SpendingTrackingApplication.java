fce00f0fd08bfe03a13e8993e0c085a8ed6cb65d
Sethon <sethon@Sethons-MacBook-Pro.local>
2025-04-10 22:04:47 -0700

    init

---
+++ src/main/java/com/connordev/spending_tracking/SpendingTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.connordev.spending_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendingTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendingTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/connordev/spending_tracking/SpendingTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connordev.spending_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendingTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
